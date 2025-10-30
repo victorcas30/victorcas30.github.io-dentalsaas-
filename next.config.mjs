52cf75c5a1b2e56054accdda34a61322e05738f3
Victor Castillo <vcastillo.mancia@gmail.com>
2025-10-30 09:48:51 -0600

    Github Page
    OK

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  basePath: '/victorcas30.github.io-dentalsaas-',
+  assetPrefix: '/victorcas30.github.io-dentalsaas-/',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+};
 
 export default nextConfig;
